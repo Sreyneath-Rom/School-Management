f490b46ff278ce5688a2a580bf4c83588dc0434e
Sreyneath <romsreyneath4@gmail.com>
2026-08-26 13:49:59 +0700

    delete glass in header

--- high-school-admin/src/layouts/Header.tsx
+++ high-school-admin/src/layouts/Header.tsx
@@ -276,8 +276,8 @@ export default function Header({ onOpenSidebar }: { onOpenSidebar?: () => void }
   }
 
   return (
-    // <-- CHANGED: added glass-sm to the header
-    <header className="sticky top-0 z-30 glass-sm">
+
+    <header className="sticky top-0 z-30 ">
       <div className="flex h-20 items-center justify-between gap-3 px-4 sm:px-6 lg:px-8">
         {/* LEFT */}
         <div className="flex min-w-0 items-center gap-3">
